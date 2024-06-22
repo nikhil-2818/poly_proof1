380b1e857514a5bf2f0516afe5113274cf427747
nikhil-2818 <156065283+nikhil-2818@users.noreply.github.com>
2024-06-23 00:41:50 +0530

    deployModule.js

--- ignition/modules/deployModule.js
+++ ignition/modules/deployModule.js
@@ -1,9 +1,21 @@
-const { buildModule } = require("@nomicfoundation/hardhat-ignition/modules");
+// We require the Hardhat Runtime Environment explicitly here. This is optional
+// but useful for running the script in a standalone fashion through `node <script>`.
+//
+// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
+// will compile your contracts, add the Hardhat Runtime Environment's members to the
+// global scope, and execute the script.
+const hre = require("hardhat");
 
-module.exports = buildModule("rainyModule", (m) => {
-  const rainy = m.contract("rainy");
-  return { rainy };
+async function main() {
+
+  const token = await hre.ethers.deployContract("rainy");
+
+  console.log("Token address:", await token.getAddress());
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
 });
-
-// yarn hardhat ignition deploy ./ignition/modules/deployModule.js --network sepolia
-// yarn hardhat verify DeployedAddress --network sepolia
